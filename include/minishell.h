bd12f18057d47e16b635f65d7899e9d59146d30b
bsomers42 <brittsomers22@gmail.com>
2022-07-21 15:36:12 +0200

    To branch parser

--- include/minishell.h
+++ include/minishell.h
@@ -6,25 +6,38 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/19 15:26:56 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/07/19 15:47:43 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/07/21 15:35:30 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "libft.h"
+
 /*
-	t_part stores information about one part of the user input
-	@str contains one part of the input sentence. 
-	@ls contains the left border sign of this part.
-	@rs contains the right border sign of this part.
-	Possible borders: | < > [ ] 0
+	t_part stores information about one part of the user input seperated by pipes
+	@in contains the name of the inputfile. 
+	@cmd contains the command to be executed. 
+	@out contains the name of the outputfile. 
+	@in_r shows the sign before the infile. < or [
+	@out_r shows the sign before the outfile. > or ]
 */
 typedef struct s_part
 {
-	char	*str;
-	char	lb;
-	char	rb;
+	char	*in;
+	char	*cmd;
+	char	*out;
+	char	in_r;
+	char	out_r;
 }	t_part;
+
+char *readline (const char *prompt);
 
 #endif
---
+++ src/parse.c
@@ -0,0 +1,201 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parse.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/07/19 14:08:32 by bsomers       #+#    #+#                 */
+/*   Updated: 2022/07/20 16:14:03 by bsomers       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	print_info(t_part *parts)
+{
+	printf("START\n");
+	printf("lb: %c\n", parts->lb);
+	printf("str: %s\n", parts->str);
+	printf("rb: %c\n", parts->rb);
+	printf("END\n");
+}
+
+int	check_redirections_pipe(char *str)
+{
+	if (ft_strncmp(str, "<\0", 2) == 0 || ft_strncmp(str, ">\0", 2) == 0 || 
+	ft_strncmp(str, "|\0", 2) == 0 || ft_strncmp(str, "<<\0", 3) == 0 || ft_strncmp(str, ">>\0", 3) == 0)
+		return (0);
+	else
+		return (1);
+}
+
+int	count_redirections_pipe(char *str)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '<' && str[i+1] == '<')
+		{
+			j++;
+			i = i + 2;
+		}
+		else if (str[i] == '>' && str[i+1] == '>')
+		{
+			j++;
+			i = i + 2;
+		}
+		else if (str[i] == '>' || str[i] == '<' || str[i] == '|')
+		{
+			j++;
+			i++;
+		}
+		else
+			i++;
+	}
+	return (j);
+}
+
+int	count_pipes(char *str)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '|')
+		{
+			j++;
+			i++;
+		}
+		else
+			i++;
+	}
+	return (j);
+}
+
+char	set_b(char *str)
+{
+	if (ft_strncmp(str, "<<\0", 3) == 0)
+		return ('[');
+	else if (ft_strncmp(str, ">>\0", 3) == 0)
+		return(']');
+	else
+		return(str[0]);
+}
+
+void	make_parts(t_part *part, char *str)
+{
+	if (ft_strncmp(str, "<\0", 2) == 0)
+		part->out_r = '<';
+	else if (ft_strncmp(str, ">\0", 2) == 0)
+		part->in_r = '>';
+	else if (ft_strncmp(str, "<<\0", 3) == 0)
+		part->out_r = '[';
+	else if (ft_strncmp(str, ">>\0", 3) == 0)
+		part->in_r = ']';
+}
+
+void	set_zero_parts(t_part *part)
+{
+	ft_bzero(part->in, 1);
+	ft_bzero(part->cmd, 1);
+	ft_bzero(part->out, 1);
+	part->in_r = '\0';
+	part->out_r = '\0';
+}
+
+void	exec_minishell(char *input)
+{
+	char **input_split;
+	t_part *parts;
+	int	count_red;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	if (ft_strchr(input, '|') != NULL)
+		input_split = ft_split(input, '|'); //REKENING HOUDEN MET PIPE TUSSEN QUOTES
+	//input_split = ft_split(input, ' ');
+	count_pipe = count_pipes(input);
+	parts = malloc((count_pipe + 1) * sizeof(t_part *));
+	while (i < (count_pipe + 1))
+	{
+		set_zero_parts(parts[i]);
+		make_parts(parts[i], input_split[i]);
+		i++;
+	}
+	i = 0;
+
+
+
+	while (check_redirections_pipe(input_split[i]) != 0 && input_split[i] != NULL)
+	{
+		if (i == 0)
+			parts[0].str = ft_strdup(input_split[i]);
+		else
+			parts[0].str = ft_strjoin(parts[0].str, input_split[i]);
+		i++;
+	}
+	parts[0].lb = 0;
+	parts[0].rb = set_b(input_split[i]);
+	while (input_split[i] != NULL)
+	{
+		if (ft_strncmp(input_split[i], "<<\0", 3) == 0)
+			parts[j].lb = '[';
+		else if (ft_strncmp(input_split[i], ">>\0", 3) == 0)
+			parts[j].lb = ']';
+		else
+			parts[j].lb = *input_split[i]; //Check for quotes!!
+		i++;
+		if (check_redirections_pipe(input_split[i]) != 0)
+			parts[j].str = ft_strdup(input_split[i]);
+		while (check_redirections_pipe(input_split[i]) != 0 && input_split[i] != NULL)
+		{
+			parts[j].str = ft_strjoin(parts[j].str, input_split[i]);
+			i++;
+		}
+		if (ft_strncmp(input_split[i], "<<\0", 3) == 0)
+			parts[j].rb = '[';
+		else if (ft_strncmp(input_split[i], ">>\0", 3) == 0)
+			parts[j].rb = ']';
+		else
+			parts[j].rb = *input_split[i]; //Check for quotes!!
+		print_info(&parts[j]);
+		j++;
+	}
+	parts[j].str = NULL;
+	parts[j].lb = '\0';
+	parts[j].rb = '\0';
+}
+
+void	run_minishell()
+{
+	char *str;
+	int	cmp;
+
+	cmp = -1;
+	while (cmp != 0)
+	{
+		str = readline("mickeyshell>");
+		cmp = ft_strncmp(str, "exit\0", 5);
+		if (cmp == 0)
+			break ;
+		exec_minishell(str);
+		free (str);
+	}
+	free (str);
+	//exit_minishell();
+}
+
+int	main()
+{
+	run_minishell();
+}
