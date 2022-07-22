dd747f07d4f43f4840d3e4c411070f5d7a8922d2
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-07-22 15:41:57 +0200

    main not to be added to master

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/21 15:10:44 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/07/22 13:31:44 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/07/22 14:44:26 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,16 @@
 
 int	main(int argc, char **argv, char **env)
 {
+	// remove below
 	argc++;
 	(void)argv;
-	create_global(env);
+	//remove above
+	init_global(env);
 	printf("%s\n", g_info.paths[0]);
+	execute_pwd();
+	set_env_variable("PWD=/hoi/dit/is/een/test");
+	execute_pwd();
+	set_env_variable("PWT=/hoi/dit/is/ook/een/test");
+	printf("%s\n", get_env_variable("PWT="));
 	return (0);
 }
