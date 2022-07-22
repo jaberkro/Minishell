626eafca3116a78676da5888e4eff5333e986a70
bsomers42 <brittsomers22@gmail.com>
2022-07-22 15:23:52 +0200

    Basic functioning parser

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/19 14:08:32 by bsomers       #+#    #+#                 */
-/*   Updated: 2022/07/21 16:44:10 by bsomers       ########   odam.nl         */
+/*   Updated: 2022/07/22 15:20:10 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,49 +15,12 @@
 void	print_info(t_part *parts)
 {
 	printf("START\n");
-	printf("lb: %c\n", parts->lb);
-	printf("str: %s\n", parts->str);
-	printf("rb: %c\n", parts->rb);
+	printf("infile: %s\n", parts->in);
+	printf("cmd: %s\n", parts->cmd);
+	printf("outfile: %s\n", parts->out);
+	printf("in_r: %c\n", parts->in_r);
+	printf("out_r: %c\n", parts->out_r);
 	printf("END\n");
-}
-
-int	check_redirections_pipe(char *str)
-{
-	if (ft_strncmp(str, "<\0", 2) == 0 || ft_strncmp(str, ">\0", 2) == 0 || 
-	ft_strncmp(str, "|\0", 2) == 0 || ft_strncmp(str, "<<\0", 3) == 0 || ft_strncmp(str, ">>\0", 3) == 0)
-		return (0);
-	else
-		return (1);
-}
-
-int	count_redirections_pipe(char *str)
-{
-	int	i;
-	int j;
-
-	i = 0;
-	j = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == '<' && str[i+1] == '<')
-		{
-			j++;
-			i = i + 2;
-		}
-		else if (str[i] == '>' && str[i+1] == '>')
-		{
-			j++;
-			i = i + 2;
-		}
-		else if (str[i] == '>' || str[i] == '<' || str[i] == '|')
-		{
-			j++;
-			i++;
-		}
-		else
-			i++;
-	}
-	return (j);
 }
 
 int	count_pipes(char *str)
@@ -80,135 +43,104 @@ int	count_pipes(char *str)
 	return (j);
 }
 
-char	set_b(char *str)
-{
-	if (ft_strncmp(str, "<<\0", 3) == 0)
-		return ('[');
-	else if (ft_strncmp(str, ">>\0", 3) == 0)
-		return(']');
-	else
-		return(str[0]);
-}
-
-void	make_parts_cmd(t_part *part, char *str)
-{
-
-}
-
-void	make_parts_file(t_part *part, char *str)
+void	assign_parts(t_part *part, char *str)
 {
 	int	i;
+	int	start;
+	int	len;
 
 	i = 0;
-	while (i < (ft_strlen(str)))
+	start = 0;
+	len = 0;
+	while (i < ((int)ft_strlen(str)))
 	{
-		//HIERRRRRR
-		if (ft_strncmp(str[i], "<", 1) == 0)
-			part->out_r = '<';
-		else if (ft_strncmp(str[i], ">", 1) == 0)
+		//printf("Str to be checked: %s\n", str);
+		if (str[i] == '>' && str[i + 1] != '>' && str[i - 1] != '>')//dus woord hierna is outfile
+		{
 			part->in_r = '>';
-		else if (ft_strncmp((str[i] + str[i+1]), "<<", 2) == 0)
-			part->out_r = '[';
-		else if (ft_strncmp((str[i] + str[i+1]), ">>", 2) == 0)
+			str[i] = ' ';
+			while (ft_isspace(str[i]) != 0)
+				i++;
+			len = calc_len_word_after(str, i);
+			part->out = ft_substr(str, i, len);
+			part->out = ft_strtrim(part->out, " "); //Voor het geval meerdere spaties tussen de woorden zaten
+			str = set_space(str, i, len);
+			i = i + len;
+		}
+		else if (str[i] == '>' && str[i + 1] == '>') //dus woord hierna is outfile
+		{
 			part->in_r = ']';
-		i++;
-	}
-}
-
-void	make_parts_r(t_part *part, char *str)
-{
-	int	i;
-
-	i = 0;
-	while (i < (ft_strlen(str)))
-	{
-		if (ft_strncmp(str[i], "<", 1) == 0)
+			str[i] = ' ';
+			str[i + 1] = ' ';
+			while (ft_isspace(str[i]) != 0)
+				i++;
+			len = calc_len_word_after(str, i);
+			part->out = ft_substr(str, i, len);
+			str = set_space(str, i, len);
+			i = i + len;
+		}
+		else if (str[i] == '<' && str[i + 1] != '<' && str[i - 1] != '<')//dus woord hierna is infile
+		{
 			part->out_r = '<';
-		else if (ft_strncmp(str[i], ">", 1) == 0)
-			part->in_r = '>';
-		else if (ft_strncmp((str[i] + str[i+1]), "<<", 2) == 0)
+			str[i] = ' ';
+			while (ft_isspace(str[i]) != 0)
+				i++;
+			len = calc_len_word_after(str, i);
+			part->in = ft_substr(str, i, len);
+			str = set_space(str, i, len);
+			i = i + len;
+		}
+		else if (str[i] == '<' && str[i + 1] == '<') //Bij heredoc <<, dus woord hierna is stopwoord
+		{
 			part->out_r = '[';
-		else if (ft_strncmp((str[i] + str[i+1]), ">>", 2) == 0)
-			part->in_r = ']';
-		i++;
+			str[i] = ' ';
+			str[i + 1] = ' ';
+			while (ft_isspace(str[i]) != 0)
+				i++;
+			len = calc_len_word_after(str, i);
+			str = set_space(str, i, len);
+			i = i + len;
+		}
+		else
+			i++;
+		len = 0;
+		//printf("Main string after setting spaces: -%s-\n", str);
 	}
+	part->cmd = ft_strdup(str);
 }
 
 void	set_zero_parts(t_part *part)
 {
-	ft_bzero(part->in, 1);
-	ft_bzero(part->cmd, 1);
-	ft_bzero(part->out, 1);
-	part->in_r = '\0';
-	part->out_r = '\0';
+	part->in = NULL;
+	part->out = NULL;
+	part->cmd = NULL;
+	part->in_r = 0;
+	part->out_r = 0;
 }
 
 void	exec_minishell(char *input)
 {
 	char **input_split;
 	t_part *parts;
-	int	count_red;
+	int	fd;
+	int	count_pipe;
 	int	i;
-	int	j;
 
 	i = 0;
-	j = 1;
-	if (ft_strchr(input, '|') != NULL)
-		input_split = ft_split(input, '|'); //REKENING HOUDEN MET PIPE TUSSEN QUOTES
-	//input_split = ft_split(input, ' ');
+	fd = dup(0);
+	input_split = ft_split(input, '|'); //REKENING HOUDEN MET PIPE TUSSEN QUOTES
 	count_pipe = count_pipes(input);
-	parts = malloc((count_pipe + 1) * sizeof(t_part *));
+	parts = malloc((count_pipe + 1) * sizeof(t_part));
+	if (parts == NULL)
+		return ;
 	while (i < (count_pipe + 1))
 	{
-		set_zero_parts(parts[i]);
-		make_parts_r(parts[i], input_split[i]);
-		///HIERONDER
-		make_parts_cmd(parts[i], input_split[i]);
-		make_parts_file(parts[i], input_split[i]);
+		set_zero_parts(&parts[i]);
+		assign_parts(&parts[i], input_split[i]);
+		print_info(&parts[i]);
 		i++;
 	}
-	i = 0;
-
-
-
-	while (check_redirections_pipe(input_split[i]) != 0 && input_split[i] != NULL)
-	{
-		if (i == 0)
-			parts[0].str = ft_strdup(input_split[i]);
-		else
-			parts[0].str = ft_strjoin(parts[0].str, input_split[i]);
-		i++;
-	}
-	parts[0].lb = 0;
-	parts[0].rb = set_b(input_split[i]);
-	while (input_split[i] != NULL)
-	{
-		if (ft_strncmp(input_split[i], "<<\0", 3) == 0)
-			parts[j].lb = '[';
-		else if (ft_strncmp(input_split[i], ">>\0", 3) == 0)
-			parts[j].lb = ']';
-		else
-			parts[j].lb = *input_split[i]; //Check for quotes!!
-		i++;
-		if (check_redirections_pipe(input_split[i]) != 0)
-			parts[j].str = ft_strdup(input_split[i]);
-		while (check_redirections_pipe(input_split[i]) != 0 && input_split[i] != NULL)
-		{
-			parts[j].str = ft_strjoin(parts[j].str, input_split[i]);
-			i++;
-		}
-		if (ft_strncmp(input_split[i], "<<\0", 3) == 0)
-			parts[j].rb = '[';
-		else if (ft_strncmp(input_split[i], ">>\0", 3) == 0)
-			parts[j].rb = ']';
-		else
-			parts[j].rb = *input_split[i]; //Check for quotes!!
-		print_info(&parts[j]);
-		j++;
-	}
-	parts[j].str = NULL;
-	parts[j].lb = '\0';
-	parts[j].rb = '\0';
+	//HIER EXECUTER AANROEPEN: bijv. executer(0, i, fd, parts);
 }
 
 void	run_minishell()
@@ -219,7 +151,7 @@ void	run_minishell()
 	cmp = -1;
 	while (cmp != 0)
 	{
-		str = readline("mickeyshell>");
+		str = readline("mickeyshell> ");
 		cmp = ft_strncmp(str, "exit\0", 5);
 		if (cmp == 0)
 			break ;
@@ -227,7 +159,6 @@ void	run_minishell()
 		free (str);
 	}
 	free (str);
-	//exit_minishell();
 }
 
 int	main()
--- src/parse_utils.c
+++ src/parse_utils.c
@@ -6,8 +6,43 @@
 /*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/20 10:17:48 by bsomers       #+#    #+#                 */
-/*   Updated: 2022/07/22 10:01:07 by bsomers       ########   odam.nl         */
+/*   Updated: 2022/07/22 15:23:09 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	ft_isred(char c)
+{
+	if (c == '>' || c == '<')
+		return (c);
+	return (0);
+}
+
+int	calc_len_word_after(char *str, int i)
+{
+	int	count;
+
+	count = 0;
+	while (ft_isspace(str[i]) != 0)
+		i++;
+	while (str[i] != '\0' && ft_isspace(str[i]) == 0 && ft_isred(str[i]) == 0)
+	{
+		i++;
+		count++;
+	}
+	return (count);
+}
+
+char	*set_space(char *str, int start, int len)
+{
+	int	i;
+
+	i = start;
+	while (i < (len + start))
+	{
+		str[i] = ' ';
+		i++;
+	}
+	return (str);
+}
