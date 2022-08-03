1930723d2330cb26b4021dc15b128f78deeac9bf
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-08-03 14:32:12 +0200

    exit in the middle of pipes working (should not do anything)

--- src/builtin.c
+++ src/builtin.c
@@ -6,25 +6,13 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/22 14:04:02 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/07/22 19:12:51 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/08/03 13:40:32 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 #include <stdio.h>
-
-void	execute_history(void)
-{
-	printf("showing history...\n");
-	exit(0);
-}
-
-void	execute_exit(void)
-{
-	printf("exiting...\n");
-	exit(0);
-}
 
 void	execute_unset(char *command)
 {
@@ -35,7 +23,7 @@ void	execute_unset(char *command)
 void	execute_export(char *command, int max)
 {
 	if (ft_strchr(command, '=') && max == 1)
-		printf("Exporting %s...\n", command);
+		printf("exporting %s...\n", command);
 	exit(0);
 }
 
@@ -52,12 +40,10 @@ void	find_builtin_function(char *command, int max)
 	first_command = protected_split(command, ' ')[0];
 	if (ft_strncmp(first_command, "cd", 3) == 0)
 		execute_cd(protected_split(command, ' ')[1]);
-	if (ft_strncmp(first_command, "export", 3) == 0)
+	if (ft_strncmp(first_command, "export", 7) == 0)
 		execute_export(protected_split(command, ' ')[1], max);
-	if (ft_strncmp(first_command, "unset", 3) == 0)
+	if (ft_strncmp(first_command, "unset", 6) == 0)
 		execute_unset(protected_split(command, ' ')[1]);
-	if (ft_strncmp(first_command, "exit", 3) == 0)
-		execute_exit();
-	if (ft_strncmp(first_command, "history", 3) == 0)
-		execute_history();
+	if (ft_strncmp(first_command, "exit", 5) == 0)
+		exit(0);
 }
