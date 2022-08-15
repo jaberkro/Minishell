a9bb9928c56bb83420337a684f610fe93a787462
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-08-15 20:24:51 +0200

    unset working

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,16 +6,29 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/15 15:28:44 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/08/15 18:33:44 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/08/15 20:24:19 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <stdio.h> //weghalen
 
+/**
+ * @brief removes all commands from the env list except commands[0]
+ * 
+ * @param commands 
+ * @param max 
+ * @return int 
+ */
 int	execute_unset(char **commands, int max)
 {
+	int	i;
+
+	i = 1;
 	(void)max;
-	printf("unsetting %s...\n", commands[1]);
+	while (commands[i])
+	{
+		unset_env_variable(commands[i]);
+		i++;
+	}
 	return (0);
 }
