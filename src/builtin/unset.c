817acbe01d03bcfba1733c4ab893d156c98ae024
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-08-17 17:01:23 +0200

    export and unset PATH updates g_info.paths now

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,11 +6,12 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/15 15:28:44 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/08/15 20:24:19 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/08/17 15:06:32 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "libft.h"
 
 /**
  * @brief removes all commands from the env list except commands[0]
@@ -28,6 +29,11 @@ int	execute_unset(char **commands, int max)
 	while (commands[i])
 	{
 		unset_env_variable(commands[i]);
+		if (ft_strncmp(commands[i], "PATH", 5) == 0 && g_info.paths)
+		{
+			free_array(g_info.paths);
+			g_info.paths = NULL;
+		}
 		i++;
 	}
 	return (0);
