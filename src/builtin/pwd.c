719c057b3081dce408acf25f515a9f6be7360058
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-08-22 17:54:01 +0200

    added block comment

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,13 +6,20 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/15 15:28:52 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/08/16 14:59:00 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/08/22 17:53:31 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 
+/**
+ * @brief prints the current working directory
+ * 
+ * @param commands 	not used but sent from jumptable
+ * @param max 		not used but sent from jumptable
+ * @return int 
+ */
 int	execute_pwd(char **commands, int max)
 {
 	char	cwd[256];
